73361b45d666f53a35e4da115b583e20e4a683c4
Colin Crovella <cccrovella@crimson.ua.edu>
2024-05-01 14:18:34 +0200

    Cleaned up doEverything

--- MyHcalAnlzr/doEverything.sh
+++ MyHcalAnlzr/doEverything.sh
@@ -3,23 +3,7 @@ lumi=$2
 floatday=$3
 run=$4
 
-#run="363427"
-#days="39"
-#lumi="0.0"
-#floatday="08.02"
-
-
 # HcalNano step
-#cd python
-#sed -i "s/XXXXXX/${run}/g" localrun_singleFull.py
-#cmsRun localrun_singleFull.py
-#sed -i "s/${run}/XXXXXX/g" localrun_singleFull.py
-#if [[ "$pedrun" != "XXX" ]]; then
-#  sed -i "s/XXXXXX/${pedrun}/g" localrun_singlePed.py
-#  cmsRun localrun_singlePed.py
-#  sed -i "s/${pedrun}/XXXXXX/g" localrun_singlePed.py
-#fi
-#cd ..
 python3 FindDatasetToRun.py ${floatday} ${run}
 
 
@@ -35,4 +19,3 @@ hadd hist_CalibOutput_hadd.root hist_CalibOutputSummary_run*.root
 
 # Make plots: Make trends from values saved in savefile, and get example histograms from rootfile
 source makePlots.sh
-#mv /eos/user/d/dmroy/www/plots_archive/PED_plots/extrapolations/ExtrapolationFC_H* /eos/user/d/dmroy/www/plots_archive/PED_plots/extrapolations_FC/
