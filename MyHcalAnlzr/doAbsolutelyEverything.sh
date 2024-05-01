d7fea1a789e66b1fcad9dd3ad09f0da18f8797bf
Colin Crovella <cccrovella@crimson.ua.edu>
2024-05-01 14:20:30 +0200

    Updates

--- MyHcalAnlzr/doAbsolutelyEverything.sh
+++ MyHcalAnlzr/doAbsolutelyEverything.sh
@@ -1,196 +1,44 @@
-# Daysince, Lumi, Floatday, Run (optional)
+# --- Start Run2024A (First beams)
 
-# Luminosities taken close to the middle of a corresponding run, otherwise at ~12:00 of each day
-# --- Start Run2023A (First beams)
-#source doEverything.sh 0 0.0 06.04
-#source doEverything.sh 1 0.0 07.04
-#source doEverything.sh 2 0.0 08.04
-#source doEverything.sh 3 0.0 09.04
-#source doEverything.sh 4 0.0 10.04
-#source doEverything.sh 5 0.0 11.04
-#source doEverything.sh 6 0.0 12.04
-#source doEverything.sh 7 0.0 13.04
-#source doEverything.sh 8 0.0 14.04
-#source doEverything.sh 9 0.0 15.04
-#source doEverything.sh 10 0.0 16.04
-#source doEverything.sh 11 0.0 17.04
-#source doEverything.sh 12 0.0 18.04
-#source doEverything.sh 13 0.0 19.04
-#source doEverything.sh 14 0.0 20.04
-# --- End Run2023A , Start Run2023B (First 13.6 TeV collisions)
-#source doEverything.sh 15 0.0 21.04
-#source doEverything.sh 16 0.002 22.04 366451
-#source doEverything.sh 17 0.013 23.04 366495
-#source doEverything.sh 18 0.041 24.04 366504
-#source doEverything.sh 19 0.043 25.04 366533
-#source doEverything.sh 20 0.043 26.04
-#source doEverything.sh 21 0.043 27.04 
-#source doEverything.sh 22 0.093 28.04 366727
-#source doEverything.sh 23 0.138 29.04 366801
-#source doEverything.sh 24 0.292 30.04 366833
-#source doEverything.sh 25 0.383 01.05 366873
-#source doEverything.sh 26 0.696 02.05 366895
-#source doEverything.sh 27 0.873 03.05 366933
-#source doEverything.sh 28 0.985 04.05
-#source doEverything.sh 29 1.012 05.05 367064
-# --- End Run2023B , Start Run2023C (First 1200b beams)
-#source doEverything.sh 30 1.292 06.05 367095
-#source doEverything.sh 31 1.790 07.05 367112
-#source doEverything.sh 32 2.172 08.05 367133
-#source doEverything.sh 33 2.487 09.05 367232
-#source doEverything.sh 34 3.389 10.05 367270
-#source doEverything.sh 35 4.124 11.05 367337
-#source doEverything.sh 36 4.383 12.05 367406
-#source doEverything.sh 37 4.921 13.05 367415
-#source doEverything.sh 38 5.169 14.05 367475
-#source doEverything.sh 39 5.739 15.05 367515
-#source doEverything.sh 40 6.579 16.05 367554
-#source doEverything.sh 41 6.914 17.05 367618
-#source doEverything.sh 42 7.784 18.05 367696 # Used to update ZS/DPG tables (Fill 8786, avg inst. lumi.: 14317 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 43 8.163 19.05 367730
-#source doEverything.sh 44 8.380 20.05 367742
-#source doEverything.sh 45 8.590 21.05 367758
-#source doEverything.sh 46 8.959 22.05 367831
-#source doEverything.sh 47 9.453 23.05 367838 # Used to compare (Fill 8817, avg inst. lumi.: 16647 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 48 10.467 24.05 367884
-#source doEverything.sh 49 11.197 25.05 367910
-#source doEverything.sh 53 11.198 29.05 368115 # Cosmics
-#source doEverything.sh 54 11.285 30.05 368223
-#source doEverything.sh 55 11.396 31.05 368229
-#source doEverything.sh 56 11.602 01.06 368321 # Used to compare (Fill 8858, avg inst. lumi.: 10966 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 57 12.138 02.06 368382
-#source doEverything.sh 58 12.444 03.06 368388
-#source doEverything.sh 59 12.944 04.06 368410
-#source doEverything.sh 60 14.188 05.06 368489
-#source doEverything.sh 61 14.884 06.06 368547 # Used to compare (Fill 8880, avg inst. lumi.: 14213 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 62 15.473 07.06 368566
-#source doEverything.sh 63 16.030 08.06 368609
-#source doEverything.sh 64 16.905 09.06 368670
-#source doEverything.sh 65 17.665 10.06 368685
-#source doEverything.sh 66 18.734 11.06 368753
-#source doEverything.sh 67 19.304 12.06 368765
-#source doEverything.sh 68 19.304 13.06 368822 # Used to compare (Fill 8901, avg inst. lumi.: 18670 * 10^30 cm^-2 s^-1)
-# --- MD/TS 1 -> Cosmics
-#source doEverything.sh 69 19.8939 14.06 368851
-#source doEverything.sh 72 19.894 17.06 369047
-#source doEverything.sh 74 19.894 19.06 369146
-#source doEverything.sh 75 19.894 20.06 369201
-#source doEverything.sh 76 19.894 21.06 369255
-#source doEverything.sh 77 19.894 22.06 369384
-#source doEverything.sh 78 19.894 23.06 369391
-#source doEverything.sh 79 19.894 24.06 369537
-#source doEverything.sh 80 19.894 25.06 369585
-#source doEverything.sh 81 19.894 26.06 369679
-#source doEverything.sh 82 19.894 27.06 369773
-# --- VdM scans
-#source doEverything.sh 83 19.8942 28.06 369794
-#source doEverything.sh 84 19.8946 29.06 369802
-# --- End Run2023C , Start Run2023D (Ramp up after MDTS1)
-#source doEverything.sh 85 19.909 30.06 369870
-#source doEverything.sh 86 19.925 01.07 369927
-#source doEverything.sh 87 20.181 02.07 369978
-#source doEverything.sh 88 20.302 03.07 369998
-#source doEverything.sh 89 20.870 04.07 370102
-#source doEverything.sh 90 21.373 05.07 370144 # Used to compare (Fill 9031, avg inst. lumi.: 14740 * 10^30 cm^-2 s^-1) -> Has some bad feature
-#source doEverything.sh 91 22.715 06.07 370175 # Used to update ZS/DPG tables (Fill 9036, avg inst. lumi.: 16961 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 92 23.301 07.07 370293
-#source doEverything.sh 93 23.976 08.07 370300
-#source doEverything.sh 94 25.341 09.07 370340
-#source doEverything.sh 95 26.256 10.07 370406
-#source doEverything.sh 96 26.860 11.07 370497 # Used to compare (Fill 9057, avg inst. lumi.: 17892 * 10^30 cm^-2 s^-1)
-#source doEverything.sh 97 27.151 12.07 370560
-#source doEverything.sh 98 27.898 13.07 370580
-#source doEverything.sh 99 28.160 14.07 370667
-#source doEverything.sh 100 28.335 15.07 370725
-#source doEverything.sh 101 29.529 16.07 370772
-# --- LHC issue -> Cosmics
-#source doEverything.sh 102 29.6369 17.07 370825
-#source doEverything.sh 103 29.637 18.07 370883
-#source doEverything.sh 104 29.637 19.07 370904
-#source doEverything.sh 105 29.637 20.07 371004
-#source doEverything.sh 106 29.637 21.07 371109
-#source doEverything.sh 107 29.637 22.07 371120
-#source doEverything.sh 108 29.637 23.07 371136
-#source doEverything.sh 109 29.637 24.07 371149
-#source doEverything.sh 110 29.637 25.07 371208
-#source doEverything.sh 111 29.637 26.07 371230
-#source doEverything.sh 112 29.637 27.07 371306
-#source doEverything.sh 137 29.637 21.08 371690
-#source doEverything.sh 138 29.637 22.08 371867
-#source doEverything.sh 139 29.637 23.08 371949
-#source doEverything.sh 140 29.637 24.08 372100
-#source doEverything.sh 141 29.637 25.08 372102
-#source doEverything.sh 142 29.637 26.08 372297
-#source doEverything.sh 143 29.637 27.08 372316
-#source doEverything.sh 144 29.637 28.08 372374
-# --- End Run2023D , Start Run2023E (First beam after LHC issue) -> Need to use CMSSW_13_2_2
-#source doEverything.sh 145 29.637 29.08 372437
-#source doEverything.sh 146 29.637 30.08 372481
-#source doEverything.sh 147 29.637 31.08 372552
-# --- Back to collisions key
-#source doEverything.sh 148 29.637 01.09 372602
-#source doEverything.sh 149 29.637 02.09 372658
-#source doEverything.sh 150 29.637 03.09 372711
-#source doEverything.sh 151 29.637 04.09 372761
-#source doEverything.sh 153 29.637 06.09 372921
-#source doEverything.sh 154 29.637 07.09 373060
-# --- End Run2023E , Start Run2023F (Very high beta* runs)
-#source doEverything.sh 158 29.637 11.09 373244
-#source doEverything.sh 159 29.637 12.09 373279
-#source doEverything.sh 160 29.637 13.09 373370
-#source doEverything.sh 161 29.637 14.09 373389
-#source doEverything.sh 162 29.637 15.09 373483
-#source doEverything.sh 164 29.637 17.09 373573
-#source doEverything.sh 165 29.637 18.09 373605 
-#source doEverything.sh 166 29.637 19.09 373664
-#source doEverything.sh 167 29.637 20.09 373779
-#source doEverything.sh 168 29.637 21.09 373785
-# --- End Run2023F, Start HIRun2023A (Heavy Ion runs)
-#source doEverything.sh 169 29.637 22.09 373960
-#source doEverything.sh 170 29.637 23.09 373985
-#source doEverything.sh 171 29.637 24.09 374119
-#source doEverything.sh 172 29.637 25.09 374188
-#source doEverything.sh 173 29.637 26.09 374289
-#source doEverything.sh 174 29.637 27.09 374335
-#source doEverything.sh 175 29.637 28.09 374361
-#source doEverything.sh 176 29.637 29.09 374507
-#source doEverything.sh 177 29.637 30.09 374574
-#source doEverything.sh 178 29.637 01.10 374613
-#source doEverything.sh 179 29.637 02.10 374666
-#source doEverything.sh 180 29.637 03.10 374709
-#source doEverything.sh 181 29.637 04.10 374730
-#source doEverything.sh 182 29.637 05.10 374803
-#source doEverything.sh 183 29.637 06.10 374833
-#source doEverything.sh 184 29.637 07.10 374868
-#source doEverything.sh 185 29.637 08.10 374873
-#source doEverything.sh 186 29.637 09.10 374896
-#source doEverything.sh 187 29.637 10.10 374961
-#source doEverything.sh 188 29.637 11.10 374997
-#source doEverything.sh 189 29.637 12.10 375055
-#source doEverything.sh 190 29.637 13.10 375064
-#source doEverything.sh 191 29.637 14.10 375145
-#source doEverything.sh 192 29.637 15.10 375195
-#source doEverything.sh 193 29.637 16.10 375252
-#source doEverything.sh 194 29.637 17.10 375300
-#source doEverything.sh 195 29.6376 18.10 375349
-#source doEverything.sh 196 29.6376 19.10 375386
-#source doEverything.sh 197 29.6376 20.10 375440
-#source doEverything.sh 198 29.6376 21.10 375483
-#source doEverything.sh 199 29.6376 22.10 375507
-#source doEverything.sh 200 29.6376 23.10 375531
-#source doEverything.sh 201 29.6376 24.10 375612
-#source doEverything.sh 202 29.6376 25.10 375631
-#source doEverything.sh 203 29.6376 26.10 375697
-#source doEverything.sh 204 29.6376 27.10 375739
-#source doEverything.sh 205 29.6376 28.10 375754
-#source doEverything.sh 206 29.6376 29.10 375823
-#source doEverything.sh 207 29.6376 30.10 375831
-# Start of 2024 (skipped a few days because calibration gap was bad until fix)
-#source doEverything.sh 1 0.0 29.02 377252
-#source doEverything.sh 2 0.0 01.03 377290
-#source doEverything.sh 3 0.0 02.03 377297
-#source doEverything.sh 5 0.0 04.03 377327
-#source doEverything.sh 6 0.0 05.03 377461
-#source doEverything.sh 7 0.0 06.03 377470
-#source doEverything.sh 8 0.0 07.03 377578
-#source doEverything.sh 10 0.0 09.03 377662
+#source doEverything.sh DAYSSINCE INT_LUMI FLOATDAY RUNNUMBER
+
+#source doEverything.sh 1 0.0 20.03 378225
+#source doEverything.sh 2 0.0 21.03 378239
+#source doEverything.sh 3 0.0 22.03 378282
+#source doEverything.sh 4 0.0 23.03 378322
+#source doEverything.sh 5 0.0 24.03 378362
+#source doEverything.sh 6 0.0 25.03 378382
+#source doEverything.sh 7 0.0 26.03 378489
+#source doEverything.sh 8 0.0 27.03 378589
+#source doEverything.sh 9 0.0 28.03 378602
+#source doEverything.sh 10 0.0 29.03 378654
+#source doEverything.sh 11 0.0 30.03 378689
+#source doEverything.sh 12 0.0 31.03 378737
+#source doEverything.sh 13 0.0 01.04 378773
+#source doEverything.sh 14 0.0 02.04 378795
+#source doEverything.sh 15 0.0 03.04 378850
+#source doEverything.sh 16 0.0 04.04 378927
+#source doEverything.sh 17 0.0 05.04 378965
+#source doEverything.sh 18 0.0 06.04 379011 
+#source doEverything.sh 19 0.003 07.04 379058 
+#source doEverything.sh 20 0.0172 08.04 379143
+#source doEverything.sh 21 0.0288 09.04 379171
+#source doEverything.sh 22 0.066 10.04 379238
+#source doEverything.sh 23 0.119 11.04 379252
+#source doEverything.sh 24 0.206 12.04 379349
+#source doEverything.sh 25 0.546 13.04 379354 
+#source doEverything.sh 26 0.710 14.04 379416
+#source doEverything.sh 27 0.928 15.04 379442
+#source doEverything.sh 28 1.391 16.04 379503
+#source doEverything.sh 29 1.583 17.04 379593
+#source doEverything.sh 30 2.504 18.04 379660
+#source doEverything.sh 31 2.779 19.04 379729 
+#source doEverything.sh 32 3.400 20.04 379765
+#source doEverything.sh 33 3.543 21.04 379774
+#source doEverything.sh 34 3.543 22.04 379830
+#source doEverything.sh 35 3.664 23.04 379866
+#source doEverything.sh 36  3.832 24.04 379954
+#source doEverything.sh 37 4.259 25.04 379984
+#source doEverything.sh 38 4.978 26.04 380030
+#source doEverything.sh 39 6.087 27.04 380049
+source doEverything.sh 40 6.684 28.04 380074
